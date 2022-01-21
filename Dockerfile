4c791c73817b7649bfc6947a52ee436d734df316
Yanlf <492254319@qq.com>
2022-01-21 15:31:26 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8
+ARG port
+
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+COPY . .
+
+EXPOSE $port
+CMD ['python', 'manage.py', 'runserver', '0.0.0.0:${port}']
---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+func() {
+        echo "Usage:"
+        echo "build.sh [-p port] [-n docker_name] [-v version]"
+        echo "Description:"
+        echo "port, the web port to be exposed."
+        echo "docker_name, the name of docker to be built."
+        echo "version, the docker version"
+        exit 0
+}
+
+
+while getopts 'n:p:v' OPT; do
+        case $OPT in
+                n) docker_name="$OPTARG";;
+                p) port="$OPTARG";;
+                v) version="$OPTARG";;
+                ?) func;;
+        esac
+done
+
+docker build -t ${docker_name:test}:${version:1.0} --build-arg port=$port -f ./Dockerfile
