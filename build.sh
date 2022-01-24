650f7e1e569e7e59b802b592008ba189fedaef05
Yanlf <492254319@qq.com>
2022-01-24 14:36:02 +0800

    remove version arg

--- build.sh
+++ build.sh
@@ -2,20 +2,19 @@
 
 func() {
   echo "Usage:"
-  echo "build.sh [-p port] [-n docker_name]"
+  echo "build.sh [-p port] [-n img_name]"
   echo "Description:"
   echo "port, the web port to be exposed."
-  echo "docker_name, the name of docker to be built."
-  echo "version, the docker version"
+  echo "img_name, the name of image to be built."
   exit 1
 }
 
 while getopts 'n:p:' OPT; do
   case $OPT in
-  n) docker_name="$OPTARG" ;;
+  n) img_name="$OPTARG" ;;
   p) port="$OPTARG" ;;
   ?) func ;;
   esac
 done
 
-docker build -t ${docker_name:=test} --build-arg in_port=${port} -f ./Dockerfile .
+docker build -t ${img_name:=test} --build-arg in_port=${port:=8686} -f ./Dockerfile .
